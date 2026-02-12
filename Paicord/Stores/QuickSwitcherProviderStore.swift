8928758fef0a82e92d9a66908bc9ce1f288c8693
Lakhan Lothiyi <llsc12.dev@gmail.com>
2026-02-12 19:21:32 +0000

    quick switcher store

---
+++ Paicord/Stores/QuickSwitcherProviderStore.swift
@@ -0,0 +1,32 @@
+//
+//  QuickSwitcherProviderStore.swift
+//  Paicord
+//
+//  Created by Lakhan Lothiyi on 12/02/2026.
+//  Copyright © 2026 Lakhan Lothiyi.
+//  
+
+import Foundation
+import PaicordLib
+import SwiftPrettyPrint
+import SwiftUIX
+
+@Observable
+class QuickSwitcherProviderStore: DiscordDataStore {
+  var gateway: GatewayStore?
+
+  var eventTask: Task<Void, Never>?
+
+  func setupEventHandling() {
+    eventTask = Task { @MainActor in
+      guard let gateway = gateway?.gateway else { return }
+      for await event in await gateway.events {
+        switch event.data {
+        default: break
+        }
+      }
+    }
+  }
+  
+  
+}
