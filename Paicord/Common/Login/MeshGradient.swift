2c1840ce3e4a0d7ddb0913ec6709273490b7d0dc
Lakhan Lothiyi <llsc12.dev@gmail.com>
2026-07-08 16:34:08 +0100

    better mesh gradient fix

--- Paicord/Common/Login/MeshGradient.swift
+++ Paicord/Common/Login/MeshGradient.swift
@@ -7,13 +7,9 @@
 //
 
 import MeshGradient
+import struct MeshGradient.Grid
 import simd
-
-import struct SwiftUI.Environment
-import struct SwiftUI.EnvironmentValues
-import struct SwiftUI.Group
-import struct SwiftUI.State
-import protocol SwiftUI.View
+import SwiftUI
 
 extension LoginView {
   struct MeshGradientBackground: View {
